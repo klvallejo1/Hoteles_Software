8076e30a36c60fedbeded25b7d6dd3436b0d2657
David Freddy <111796950+frd2377@users.noreply.github.com>
2025-06-09 19:09:48 -0500

    Delete backend/app directory

--- backend/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.database import Base
-from app.models.client import Client
-from app.models.room import Room
-from app.models.reservation import Reservation
-from app.models.invoice import Invoice
-from app.models.payment import Payment
